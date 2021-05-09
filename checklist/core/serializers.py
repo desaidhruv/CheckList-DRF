67a5e83ab3945b330815a90ca91d182ad3b72b1e
Dhruv Desai <dhruvshah28@gmail.com>
2021-05-09 16:14:23 +0530

    Added Checklist items in API Endpoint

--- checklist/core/serializers.py
+++ checklist/core/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
 from .models import CheckList, CheckListItem
 
-class CheckListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CheckList
-        fields = '__all__'
-
 class CheckListItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = CheckListItem
         fields = '__all__'
+
+class CheckListSerializer(serializers.ModelSerializer):
+    items = CheckListItemSerializer(source='checklistitem_set', many=True, read_only=True)
+    
+    class Meta:
+        model = CheckList
+        fields = '__all__'
+
